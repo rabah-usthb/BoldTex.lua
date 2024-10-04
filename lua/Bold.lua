9d4d4a0868d528581e99449c654d5cea0afe8249
rabah-usthb <chabanechaoucherabah4@gmail.com>
2024-10-04 18:09:03 +0100

    simplified code using array of string instead of string

--- lua/Bold.lua
+++ lua/Bold.lua
@@ -19,26 +19,23 @@ end
 function bold.fetchSelectedText(start_line,start_col,end_line,end_col)
   local buffer = vim.api.nvim_get_current_buf()
   local lines = vim.api.nvim_buf_get_lines(buffer,start_line-1,end_line,false)
-  local stringLines = ""
  -- print("Lines: " .. table.concat(lines, "\n"))  -- Use table.concat to join the lines
     
-  local selectedLines = ""
+  local selectedLines = {}
   if start_line == end_line  then
-    selectedLines = string.sub(lines[1],start_col,end_col)
-    stringLines = lines[1]
+    selectedLines[1] = "textbf{"..string.sub(lines[1],start_col,end_col)
   else
       for i = 1, #lines, 1 do
-          stringLines = stringLines .. lines[i] .. '\n'
        if i == 1 then
-         selectedLines = string.sub(lines[1],start_line,#lines[1])
+         selectedLines[i] = string.sub(lines[1],start_col,#lines[1])
            elseif i == (end_line - start_line + 1) then
-          selectedLines = selectedLines .. string.sub(lines[i],0,end_col) .. '\n'
+          selectedLines[i] = string.sub(lines[i],0,end_col)..'}'
             else
-                selectedLines = selectedLines .. lines[i] .. '\n'
+                selectedLines[i] = lines[i]
        end
       end
   end
-  return selectedLines,stringLines
+  return selectedLines
 end
 
 function bold.setTextBF()
@@ -49,14 +46,13 @@ function bold.setTextBF()
     local end_line = end_pos[2]
     local end_col = end_pos[3] 
     --print("sLine "..start_line.." sCol "..start_col.." eLine "..end_line.." eCol "..end_col)
-    local selectedLines , stringLines =  bold.fetchSelectedText(start_line,start_col,end_line,end_col)
-    print("String Lines : "..stringLines)
-    local newLines  = "textbf{"..selectedLines.."}"
-    local old  =newLines
-    newLines = string.sub(stringLines,0,start_col-1) .. newLines .. string.sub(stringLines,end_col+1,#stringLines)
-    print("oldNew "..old .. "\n new "..newLines)
+    local boldedLines =  bold.fetchSelectedText(start_line,start_col,end_line,end_col)
+    --print("String Lines : "..stringLines)
+    --local newLines  = "textbf{"..selectedLines.."}"
+    --newLines = string.sub(stringLines,0,start_col-1) .. newLines .. string.sub(stringLines,end_col+1,#stringLines)
+   -- print("oldNew "..old .. "\n new "..newLines)
     local buffer = vim.api.nvim_get_current_buf()
-   -- vim.api.nvim_buf_set_lines(buffer, start_line-1, end_line, false, {newLines})
+   vim.api.nvim_buf_set_lines(buffer, start_line-1, end_line, false, boldedLines)
 end
 
 function bold.main(key)
