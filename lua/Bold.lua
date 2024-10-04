2e495e03700d2b6ef87c9fc71888b975ffe4024f
rabah-usthb <chabanechaoucherabah4@gmail.com>
2024-10-04 16:18:54 +0100

    simplified code and removed constant loop that breaks nvim

--- lua/Bold.lua
+++ lua/Bold.lua
@@ -2,7 +2,7 @@ local bold = {}
 
 function bold.setKeyMap(key)
    local keyMap =  key or 'f'     
-   vim.api.nvim_set_keymap('v', keyMap, ':lua callSetTextBF()<CR>', { noremap = true, silent = true })  
+   vim.api.nvim_set_keymap('v', keyMap, ':lua callsetTextBF()<CR>', { noremap = true, silent = true })  
 end
 
 function bold.getFileExtension()
@@ -15,35 +15,6 @@ function bold.isTexFile()
     local extension = bold.getFileExtension()
     return extension == ".tex"
 end
-
-function bold.isVisualMode()
-  local mode = vim.api.nvim_get_mode().mode
-    return mode == 'v' or mode == 'V' or mode == ''  
-end
-
-function bold.setListener()
-   vim.api.nvim_create_autocmd("ModeChanged", {
-    pattern = "*:v",  -- Trigger when entering Visual mode ('v', 'V', or 'CTRL-V')
-    callback = isTextSelected,
-})
-end
-
-
- _G.isTextSelected = function()
-   repeat
-     local start_pos = vim.fn.getpos("'<")  -- Start of visual selection
-     local end_pos = vim.fn.getpos("'>")    -- End of visual selection
-     local start_line = start_pos[2]
-     local start_col = start_pos[3]
-     local end_line = end_pos[2]
-     local end_col = end_pos[3]
-        -- Check if start and end positions differ (meaning text is selected)
-        if start_col~=end_col then
-            bold.setTextBF(start_line,start_col,end_line,end_col)
-        end
-        vim.wait(100)
-        until (not bold.isVisualMode())
-    end
 
 
 function bold.fetchSelectedText(start_line,start_col,end_line,end_col)
@@ -66,16 +37,22 @@ function bold.fetchSelectedText(start_line,start_col,end_line,end_col)
   return selectedLines
 end
 
-function bold.setTextBF(start_line,start_col,end_line,end_col)
+function bold.setTextBF()
+    local start_pos = vim.fn.getpos("'<")  
+    local end_pos = vim.fn.getpos("'>")    
+    local start_line = start_pos[2]
+    local start_col = start_pos[3]
+    local end_line = end_pos[2]
+    local end_col = end_pos[3] 
     local selectedLines =  bold.fetchSelectedText(start_line,start_col,end_line,end_col)
     local newLines  = "textbf{"..selectedLines.."}"
     local buffer = vim.api.nvim_get_current_buf()
     vim.api.nvim_buf_set_lines(buffer, start_line, end_line, false, {newLines})
 end
 
-function bold.main()
-    if bold.isTexFile() then
-        bold.setListener()
+function bold.main(key)
+    if bold.isTexFile then
+        bold.setKeyMap(key)
     end
 end
 
